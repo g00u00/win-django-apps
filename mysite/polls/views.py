a3bd043ae92bc850a9ba6c9f2a39a171147b2ce9
g00u00 <g00u00@outlook.com>
2020-08-07 16:37:47 +0300

    new file:   mysite/polls/templates/polls/index.html 	modified:   mysite/polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,35 @@
 #from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 
 #def index(request):
-#    return HttpResponse("Hello, world. You're at the polls index.Кодировка. Git_001")
+#    return HttpResponse("Hello, world. You're at the polls index.")
+
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    output = ', '.join([q.question_text for q in latest_question_list])
+#    print(output)
+#    return HttpResponse(output)
+
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    template = loader.get_template('polls/index.html')
+#    context = {
+#        'latest_question_list': latest_question_list,
+#    }
+#    print("01",context)
+#    return HttpResponse(template.render(context, request))
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    print("02",context)
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
